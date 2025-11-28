007f28812beb7857897b81521e132b10c987bc26
Y0urLocalCatboy <156523842+Y0urLocalCatboy@users.noreply.github.com>
2025-11-28 11:32:52 +0100

    Test Commit

--- medtrackerapp/tests/test_views.py
+++ medtrackerapp/tests/test_views.py
@@ -2,7 +2,11 @@ from rest_framework.test import APITestCase
 from medtrackerapp.models import Medication
 from django.urls import reverse
 from rest_framework import status
-
+from unittest.mock import patch, Mock
+from rest_framework.test import APITestCase
+from medtrackerapp.models import Medication
+from django.urls import reverse
+from rest_framework import status
 
 class MedicationViewTests(APITestCase):
     def setUp(self):
@@ -16,3 +20,58 @@ class MedicationViewTests(APITestCase):
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]["name"], "Aspirin")
         self.assertEqual(response.data[0]["dosage_mg"], 100)
+
+        self.assertEqual(len(response.data), 1)
+    def test_retrieve_medication_valid_id(self):
+        url = reverse("medication-detail", args=[self.med.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["name"], "Aspirin")
+        self.assertEqual(response.data["dosage_mg"], 100)
+    def test_retrieve_medication_invalid_id(self):
+        url = reverse("medication-detail", args=[999])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    def test_list_medications_valid_data(self):
+        url = reverse("medication-list")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["name"], "Aspirin")
+        self.assertEqual(response.data[0]["dosage_mg"], 100)
+
+        self.assertEqual(len(response.data), 1)
+
+    def test_retrieve_medication_valid_id(self):
+        url = reverse("medication-detail", args=[self.med.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["name"], "Aspirin")
+        self.assertEqual(response.data["dosage_mg"], 100)
+
+    def test_retrieve_medication_invalid_id(self):
+        url = reverse("medication-detail", args=[999])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    @patch('medtrackerapp.services.DrugInfoService.get_drug_info')
+    def test_drug_info_api_mocked(self, mock_get_drug_info):
+
+        mock_response = {
+            'name': 'Aspirin',
+            'dosage_mg': 100,
+            'prescribed_per_day': 2
+        }
+        mock_get_drug_info.return_value = mock_response
+
+        url = reverse("medication-detail", kwargs={'pk': self.med.id})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'Aspirin')
+
