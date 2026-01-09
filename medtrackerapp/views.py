ca7ed7337d4780798d42b4d833a944c16ab6639c
Y0urLocalCatboy <156523842+Y0urLocalCatboy@users.noreply.github.com>
2026-01-09 12:32:46 +0100

    Part 3 of lab 7

--- medtrackerapp/views.py
+++ medtrackerapp/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Medication
 from .serializers import ExpectedDosesSerializer
+from rest_framework.filters import SearchFilter
 
 class MedicationViewSet(viewsets.ModelViewSet):
     """
